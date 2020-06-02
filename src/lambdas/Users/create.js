908366c5e16f96817a9a26a601e59400e500adc6
wayden.jones <wayden.jones1@gmail.com>
2020-06-02 19:43:26 -0400

    update project layout

--- src/lambdas/Users/create.js
+++ src/lambdas/Users/create.js
@@ -1,6 +1,13 @@
+const uuid = require ('uuid');
+const Joi = require ('@hapi/joi');
+const dynamoDB = require ('../dynamodb');
+const registerSchema = require ('../../validationSchemas/registerSchema');
+
 module.exports.handler = (event, context) => {
   try {
-    const body = event.body;
-    
+    const body = JSON.parse (event.body);
+    const timestamp = new Date ().getTime ();
+
+
   } catch (e) {}
 };
