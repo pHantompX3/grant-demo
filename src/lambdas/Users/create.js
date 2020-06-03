fa647e048a303cdb624ed909757d8ecb112f3c0a
wayden.jones <wayden.jones1@gmail.com>
2020-06-03 08:52:59 -0400

    working on the create user lambda

--- src/lambdas/Users/create.js
+++ src/lambdas/Users/create.js
@@ -2,12 +2,31 @@ const uuid = require ('uuid');
 const Joi = require ('@hapi/joi');
 const dynamoDB = require ('../dynamodb');
 const registerSchema = require ('../../validationSchemas/registerSchema');
+const timestamp = new Date ().getTime ();
+const {createParams} = require ('./db-Params/params');
 
-module.exports.handler = (event, context) => {
+module.exports.handler = async (event, context) => {
   try {
     const body = JSON.parse (event.body);
-    const timestamp = new Date ().getTime ();
+    const {joiError, value} = Joi.validate (body, registerSchema);
 
+    const result = await dynamoDB
+      .scan (createParams.emailScanParams)
+      .promise ();
 
-  } catch (e) {}
+    if (joiError) {
+      throw joiError;
+    }
+
+    if (result.Count == 0) {
+      throw new Error ('Email address already in use!');
+    }
+
+    
+  } catch (e) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify (e.details),
+    };
+  }
 };
--- src/validationSchemas/registerSchema.js
+++ src/validationSchemas/registerSchema.js
@@ -4,33 +4,35 @@ const schema = {
     tlds: {allow: true},
   }),
 
-  phone_number: Joi.string ().length (10).regex (/^\d+$/),
+  phone_number: Joi.string ().length (10).regex (/^\d+$/).required (),
 
-  password: Joi.string ().pattern (new RegExp ('^[a-zA-Z0-9]{3,30}$')),
+  password: Joi.string ()
+    .pattern (new RegExp ('^[a-zA-Z0-9]{3,30}$'))
+    .required (),
 
-  repeat_password: Joi.ref ('password'),
+  repeat_password: Joi.ref ('password').required (),
 
-  proof_id: Joi.string ().base64 (),
+  proof_id: Joi.string ().base64 ().required(),
 
-  proof_address: Joi.string ().base64 (),
+  proof_address: Joi.string ().base64 ().required(),
 
-  proof_id_type: Joi.string (),
+  proof_id_type: Joi.string ().required(),
 
-  name: Joi.string (),
+  name: Joi.string ().required(),
 
-  date_of_birth: Joi.date (),
+  date_of_birth: Joi.date ().required(),
 
-  id_number: Joi.string (),
+  id_number: Joi.string ().required(),
 
-  proof_address_type: Joi.string (),
+  proof_address_type: Joi.string ().required(),
 
-  address_line_one: Joi.string (),
+  address_line_one: Joi.string ().required(),
 
-  address_line_two: Joi.string (),
+  address_line_two: Joi.string ().required(),
 
-  address_parish: Joi.string (),
+  address_parish: Joi.string ().required(),
 
-  address_country: Joi.string (),
+  address_country: Joi.string ().required(),
 };
 
 module.exports.resgisterSchema = () => {
